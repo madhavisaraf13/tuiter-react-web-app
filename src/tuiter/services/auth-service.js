aff54eb8dae615cd60157661d4ebc7e1340d52bd
madhavisaraf13 <madhavineu23@gmail.com>
2023-08-15 10:57:09 -0400

    added server path

--- src/tuiter/services/auth-service.js
+++ src/tuiter/services/auth-service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const SERVER_API_URL = process.env.REACT_APP_SERVER_API_URL;
-//const USERS_URL = `${SERVER_API_URL}/users`;
-const USERS_URL = "http://localhost:4000/api/users"; 
+const USERS_URL = `${SERVER_API_URL}/users`;
+//const USERS_URL = "http://localhost:4000/api/users"; 
 
 const api = axios.create({ withCredentials: true });
 
